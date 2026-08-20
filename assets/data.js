aac1f1307da3a4c19e7360ce12edf9b1284b59cf
dbsgpfl9777-cmd <dbsgpfl9777@gmail.com>
2026-08-21 01:03:37 +0900

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -37,7 +37,7 @@ const PROFILE = {
     youtube: "https://youtube.com/playlist?list=PLVNH_EPvWBYzCgl5NfScfoeKlcnXQEfQl&si=RlGGMiEjheOux5Va",
     pdf: "assets/윤혜리_이력서.pdf",
   },
-  demoReel: "여기에_유튜브_링크_붙여넣기",
+  demoReel: "https://youtube.com/playlist?list=PLVNH_EPvWBYzCgl5NfScfoeKlcnXQEfQl&si=OHMglDgg_FRITlzT",
 };
 
 // ---------------- PORTFOLIO ----------------
